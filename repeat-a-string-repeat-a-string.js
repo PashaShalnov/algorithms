20b31288d457a57e7cd8611798abc05a1b58de22
Pavel Shalnov <94354424+PashaShalnov@users.noreply.github.com>
2022-04-08 23:56:14 +0300

    Add files via upload

---
+++ repeat-a-string-repeat-a-string.js
@@ -0,0 +1,12 @@
+
+function repeatStringNumTimes(str, num) {
+    if (num <= 0) {
+      return ""
+      } else {
+       return str + repeatStringNumTimes(str, num-1)
+      }  
+}
+
+
+repeatStringNumTimes("abc", 3);
+
