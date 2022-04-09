818499da194b78030ecc0c93e432c4e57b4293b8
Pavel Shalnov <94354424+PashaShalnov@users.noreply.github.com>
2022-04-09 15:01:42 +0300

    Update truncate-a-string.js

--- truncate-a-string.js
+++ truncate-a-string.js
@@ -1,10 +1,12 @@
 function truncateString(str, num) {
   
   if (num >= str.length) {
-    return str.slice(0, num)
+    return str.slice(0, num);
   } else if (num < str.length) {
-    return str.slice(0, num) + "..."
+    return str.slice(0, num) + "...";
   }
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2);
+
+//Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
